5ca0f7f03ae3fa2d31c7afb434ba64caa9dabcf1
jwright <jwright@192.168.56.1>
2017-03-27 15:35:56 -0400

    Add the ability to create a Person object and add it to the TableView

--- src/guidemo/ExampleOfTableViewController.java
+++ src/guidemo/ExampleOfTableViewController.java
@@ -19,9 +19,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.stage.Stage;
@@ -38,6 +40,11 @@ public class ExampleOfTableViewController implements Initializable {
     @FXML private TableColumn<Person, String> firstNameColumn;
     @FXML private TableColumn<Person, String> lastNameColumn;
     @FXML private TableColumn<Person, LocalDate> birthdayColumn;
+    
+    //These instance variables are used to create new Person objects
+    @FXML private TextField firstNameTextField;
+    @FXML private TextField lastNameTextField;
+    @FXML private DatePicker birthdayDatePicker;
     
     /**
      * This method will allow the user to double click on a cell and update
@@ -93,6 +100,21 @@ public class ExampleOfTableViewController implements Initializable {
         firstNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
         lastNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
     }    
+    
+    /**
+     * This method will create a new Person object and add it to the table
+     */
+    public void newPersonButtonPushed()
+    {
+        Person newPerson = new Person(firstNameTextField.getText(),
+                                      lastNameTextField.getText(),
+                                      birthdayDatePicker.getValue());
+        
+        //Get all the items from the table as a list, then add the new person to
+        //the list
+        tableView.getItems().add(newPerson);
+    }
+    
     
     
     /**
