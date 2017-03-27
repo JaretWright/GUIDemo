112acebd8efacc6bd541e4ee569f3d5bfe651a68
jwright <jwright@192.168.56.1>
2017-03-27 14:36:10 -0400

    Added a TableView object with dummy data in it.

--- src/guidemo/ExampleOfTableViewController.java
+++ src/guidemo/ExampleOfTableViewController.java
@@ -7,13 +7,21 @@ package guidemo;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 /**
@@ -23,6 +31,14 @@ import javafx.stage.Stage;
  */
 public class ExampleOfTableViewController implements Initializable {
 
+    //configure the table
+    @FXML private TableView<Person> tableView;
+    @FXML private TableColumn<Person, String> firstNameColumn;
+    @FXML private TableColumn<Person, String> lastNameColumn;
+    @FXML private TableColumn<Person, LocalDate> birthdayColumn;
+    
+    
+    
      /**
      * When this method is called, it will change the Scene to 
      * a TableView example
@@ -43,7 +59,26 @@ public class ExampleOfTableViewController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        //set up the columns in the table
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("firstName"));
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("lastName"));
+        birthdayColumn.setCellValueFactory(new PropertyValueFactory<Person, LocalDate>("birthday"));
+        
+        //load dummy data
+        tableView.setItems(getPeople());
     }    
     
+    
+    /**
+     * This method will return an ObservableList of People objects
+     */
+    public ObservableList<Person>  getPeople()
+    {
+        ObservableList<Person> people = FXCollections.observableArrayList();
+        people.add(new Person("Frank","Sinatra",LocalDate.of(1915, Month.DECEMBER, 12)));
+        people.add(new Person("Rebecca","Fergusson",LocalDate.of(1986, Month.JULY, 21)));
+        people.add(new Person("Mr.","T",LocalDate.of(1952, Month.MAY, 21)));
+        
+        return people;
+    }
 }
---
+++ src/guidemo/Person.java
@@ -0,0 +1,46 @@
+package guidemo;
+
+import java.time.LocalDate;
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ *
+ * @author jwright
+ */
+public class Person {
+    private SimpleStringProperty firstName, lastName;
+    private LocalDate birthday;
+
+    public Person(String firstName, String lastName, LocalDate birthday) {
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+        this.birthday = birthday;
+    }
+
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public void setFirstName(SimpleStringProperty firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public void setLastName(SimpleStringProperty lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    
+    
+}
